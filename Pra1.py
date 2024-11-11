ee7d2906d54090289910194ad7f9004dc144bfff
nancy325 <146329272+nancy325@users.noreply.github.com>
2024-11-11 22:34:20 +0530

    Add files via upload

---
+++ Pra1.py
@@ -0,0 +1,10 @@
+import array 
+ray('i')
+n = int(input("Entre the size of array: "))
+
+for i in range(0,n):
+
+    e = int(input("Entre the value for array: "))
+
+    (array.array('i'))rr.append(e)
+print(array.array('i'r)r)
